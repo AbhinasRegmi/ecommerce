515d5ed1824293e5999797a679529d21bbc39eb9
AbhinasRegmi <aviregmi2018@gmail.com>
2022-11-09 20:49:40 +0545

    Created Category, Product Model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+
+    #db_index=True helps in search optimization with space tradeoff
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        #in admin panel it will say categories instead of categorys
+        verbose_name_plural='categories'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='product/images/')
+
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated_at']
+
+    
+    def __str__(self):
+        return self.title
