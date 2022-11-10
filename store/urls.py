64063c7df2b715ea91e75380a57aa90c970bea6f
AbhinasRegmi <aviregmi2018@gmail.com>
2022-11-11 00:41:02 +0545

    New Views Added
    Configured different urls, views for HomeView, ProductView, CategoryView

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
+from .views import HomeView, ProductView, CategoryView
 
-
+#this name should match with that in core's urls namespace
+app_name = 'store'
 urlpatterns = [  
-    
+    path('', HomeView.as_view(), name='home-view'),
+    path('product/<slug:slug>/', ProductView.as_view(), name='product-view'),
+    path('category/<slug:slug>/', CategoryView.as_view(), name='category-view'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Category, Product
+
+
+#access this from setting templates context to send this to all pages
+def category_context(request):
+    return {
+        'category_list' : Category.objects.all().order_by('name')
+    }
+
+class HomeView(ListView):
+    context_object_name = 'product_list'
+    paginate_by = 10
+    template_name = 'store/home-view.html'
+    queryset = Product.objects.all().order_by('-updated_at')
+
+
+class ProductView(DetailView):
+    context_object_name = 'product'
+    template_name = 'store/product-view.html'
+
+    def get_queryset(self):
+        return Product.objects.filter(slug=self.kwargs.get('slug'), in_stock=True)
+
+class CategoryView(ListView):
+    context_object_name = 'product_list'
+    template_name = 'store/category-view.html'
+
+    def get_queryset(self):
+        self.category = Category.objects.filter(slug=self.kwargs.get('slug')).first()
+        return Product.objects.filter(category=self.category)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.category
+        return context
