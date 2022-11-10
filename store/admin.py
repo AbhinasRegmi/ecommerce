c0c2b22becb71f6d55952c3208bd6269f936c6af
AbhinasRegmi <aviregmi2018@gmail.com>
2022-11-10 15:34:22 +0545

    Registering Product, Category

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdminConfig(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['name']}
+
+@admin.register(Product)
+class ProductAdminConfig(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['title']}
+    list_display = ['title', 'category', 'in_stock', 'price']
+    list_filter = ['in_stock']
