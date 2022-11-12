8ce086bf16a7d3963ecc861bde103ce17754fc8e
AbhinasRegmi <aviregmi2018@gmail.com>
2022-11-12 12:51:17 +0545

    Added Basket templates, refactored othe templates

--- basket/urls.py
+++ basket/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-app_name = 'basket'
-urlpatterns = [  
+from .views import BasketSumView
 
+app_name = 'basket'
+urlpatterns = [
+    path('', BasketSumView.as_view(), name='basket-sum-view')  
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+class BasketSumView(TemplateView):
+    template_name = 'basket/basket-sum-view.html'
