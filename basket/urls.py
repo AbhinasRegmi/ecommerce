a8963bebfbb5bac5fb71b753e6c57f685ce98406
AbhinasRegmi <aviregmi2018@gmail.com>
2022-11-12 16:09:58 +0545

    Post request from Js, with csrf succed

--- basket/urls.py
+++ basket/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BasketSumView
+from .views import BasketSumView, basket_add
 
 app_name = 'basket'
 urlpatterns = [
-    path('', BasketSumView.as_view(), name='basket-sum-view')  
+    path('', BasketSumView.as_view(), name='basket-sum-view'),
+    path('add/',basket_add, name='basket-add'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,14 @@
-from django.views.generic.detail import DetailView
 from django.views.generic.base import TemplateView
+from django.http import HttpResponse
+import json
 
 class BasketSumView(TemplateView):
     template_name = 'basket/basket-sum-view.html'
+
+
+def basket_add(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        return HttpResponse(content={'data': data})
+    else:
+        return HttpResponse('get request not allowed here.')
