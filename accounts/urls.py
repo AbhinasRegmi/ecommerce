b80a518c196e4e5d0d0f65465b9a1d02cbf1144f
AbhinasRegmi <aviregmi2018@gmail.com>
2022-11-24 17:53:03 +0545

    SEPARATED DEV, PROD SETTINGS
    To achieve this i made a settings file in core, and created base, dev,
    prod. Base contains the common settings while dev contains development
    related settings, prod contains production settings. We needed to
    tell manage.py which DJANGO_SETTINGS_MODULE to use, the default was
    set to dev if we make env variable DJANGO_SETTINGS_MODULE and set to
    prod it will use production settings.

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
-    path('dashboard/<str:filter>/', DashBoardFilterView.as_view(), name='dashboard_filter'),
     path('dashboard/edit/', UpdateProfileView.as_view(), name='updateprofile' ),
+    path('dashboard/<str:filter>/', DashBoardFilterView.as_view(), name='dashboard_filter'),
     path('activate/<str:uidb64>/<str:token>', activate_view, name='activate'),
     path('reset/', ResetYourPasswordView.as_view(), name='passwordreset'),
     #using specific name uidb64 because we will be using builtin view
