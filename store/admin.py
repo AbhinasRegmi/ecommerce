e9c1ef608ce613817f9e6344027c5447fa8f046c
AbhinasRegmi <aviregmi2018@gmail.com>
2022-11-26 11:41:14 +0545

    Created new migrations, db

--- store/admin.py
+++ store/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
-from .models import Category, Product
+
+from mptt.admin import ModelAdmin
+
+from .models import (
+    Category,
+    ProductType,
+    ProductSpecification,
+    ProductSpecificationValue,
+    Product,
+    ProductImage
+)
 
 
-@admin.register(Category)
-class CategoryAdminConfig(admin.ModelAdmin):
-    list_display = ['name', 'total_items']
-    prepopulated_fields = {'slug': ['name']}
+# @admin.register(Category)
+# class CategoryAdminConfig(admin.ModelAdmin):
+#     list_display = ['name', 'total_items']
+#     prepopulated_fields = {'slug': ['name']}
 
-@admin.register(Product)
-class ProductAdminConfig(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ['title']}
-    list_display = ['title', 'category', 'in_stock', 'price']
-    list_filter = ['in_stock']
+# @admin.register(Product)
+# class ProductAdminConfig(admin.ModelAdmin):
+#     prepopulated_fields = {'slug': ['title']}
+#     list_display = ['title', 'category', 'in_stock', 'price']
+#     list_filter = ['in_stock']
--- store/models.py
+++ store/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.urls import reverse
-from django.conf import settings
 
 from mptt.models import TreeForeignKey, MPTTModel
 
